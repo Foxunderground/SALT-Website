47b19a72d302c21d3ceb5ef5f451cf815c5606fe
foxunderground <umerirfan1205@gmail.com>
2022-06-21 17:12:20 +0500

    Edited for heroku

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const router = express.Router();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+//app.set("views", path.join(__dirname, "views"));
+
+router.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.use("/", router);
+
+let port = process.env.PORT || 8081;
+var server = app.listen(port, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+	//console.log(`Example app listening at http://${host}:${port}`)
+});
+
+console.log("Running at Port 3000");
